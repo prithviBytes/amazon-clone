118a4f3473ea374520909b6087a672f39b21f677
Prithviraj Suvarna <prithvisuvarna56@gmail.com>
2021-02-09 12:34:47 +0000

    Add Firebase and Clean Files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlppyLjRAwVMhU3f7mdO_n6Q00gm3hdXs",
+  authDomain: "clone-dbd0d.firebaseapp.com",
+  projectId: "clone-dbd0d",
+  storageBucket: "clone-dbd0d.appspot.com",
+  messagingSenderId: "540886400147",
+  appId: "1:540886400147:web:fff60ee02cd4ad01f56166",
+  measurementId: "G-VBJZVGD6FW"
+};
